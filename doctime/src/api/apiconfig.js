935af60025b5bed9f8d118733ed3e202e5365cd0
MaithiliTyagi <tyagimaithili918@gmail.com>
2024-12-16 23:10:40 +0530

    apiconfig.js

--- doctime/src/api/apiconfig.js
+++ doctime/src/api/apiconfig.js
@@ -1,9 +1,11 @@
+  // Importing the Axios library for making HTTP requests
 import axios from 'axios';
 
+// Base URL for the API, typically points to the backend server
 const BASE_URL = 'http://localhost:8001/api';
 
+// Creating an Axios instance with pre-configured settings
 export const api = axios.create({
-  baseURL: BASE_URL,
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: BASE_URL, // Setting the base URL for all requests made using this instance
+  headers: { 'Content-Type': 'application/json' }, // Default headers for requests, specifying JSON format
 });
-  
